861229fedb3532748de85f204dc31ec8748275f1
MoshCat <bas@middelham.nl>
2017-05-11 19:36:46 +0200

    Homepage grid overhaul
    Fixed parallax effects

--- themes/joyofcoding/static/js/main.js
+++ themes/joyofcoding/static/js/main.js
@@ -1,43 +1,47 @@
 // @codekit-prepend 'jquery.min.js'
 // @codekit-prepend 'jquery.stellar.min.js'
 
-$.stellar({
-  // Set scrolling to be in either one or both directions
-  horizontalScrolling: false,
-  verticalScrolling: true,
-
-  // Set the global alignment offsets
-  horizontalOffset: 0,
-  verticalOffset: 0,
-
-  // Refreshes parallax content on window load and resize
-  responsive: true,
-
-  // Select which property is used to calculate scroll.
-  // Choose 'scroll', 'position', 'margin' or 'transform',
-  // or write your own 'scrollProperty' plugin.
-  scrollProperty: 'scroll',
-
-  // Select which property is used to position elements.
-  // Choose between 'position' or 'transform',
-  // or write your own 'positionProperty' plugin.
-  positionProperty: 'transform',
-
-  // Enable or disable the two types of parallax
-  parallaxBackgrounds: true,
-  parallaxElements: true,
-
-  // Hide parallax elements that move outside the viewport
-  hideDistantElements: false,
-
-  // Customise how elements are shown and hidden
-  hideElement: function($elem) { $elem.hide(); },
-  showElement: function($elem) { $elem.show(); }
-});
-
 if(document.getElementById('container').classList.contains("page_home")) {
   document.body.classList.toggle('home');
 }
+
+
+$(document).ready(function() {
+  $.stellar({
+    // Set scrolling to be in either one or both directions
+    horizontalScrolling: false,
+    verticalScrolling: true,
+  
+    // Set the global alignment offsets
+    horizontalOffset: 0,
+    verticalOffset: 200,
+  
+    // Refreshes parallax content on window load and resize
+    responsive: true,
+  
+    // Select which property is used to calculate scroll.
+    // Choose 'scroll', 'position', 'margin' or 'transform',
+    // or write your own 'scrollProperty' plugin.
+    scrollProperty: 'scroll',
+  
+    // Select which property is used to position elements.
+    // Choose between 'position' or 'transform',
+    // or write your own 'positionProperty' plugin.
+    positionProperty: 'transform',
+  
+    // Enable or disable the two types of parallax
+    parallaxBackgrounds: false,
+    parallaxElements: true,
+
+    // Hide parallax elements that move outside the viewport
+    hideDistantElements: true,
+
+    // Customise how elements are shown and hidden
+    hideElement: function($elem) { $elem.css('visibility', 'hidden'); },
+    showElement: function($elem) { $elem.css('visibility', 'visible'); }
+  });
+});
+
 
 // poster frame click event
 $(document).on('click','.js-videoPoster',function(ev) {
