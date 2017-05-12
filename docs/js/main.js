75ac5382455eb19e803120aad3064b3e1a311d12
Cies Breijs <cies AT kde DOT nl>
2017-05-12 11:23:08 +0200

    Add QWAN, regenerate

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,10 @@
 // @codekit-prepend 'jquery.min.js'
 // @codekit-prepend 'jquery.stellar.min.js'
 
+
+/*--------------------------------------------------------------
+# Stellar - https://github.com/markdalgleish/stellar.js
+--------------------------------------------------------------*/
 $.stellar({
   // Set scrolling to be in either one or both directions
   horizontalScrolling: false,
@@ -8,7 +12,7 @@ $.stellar({
 
   // Set the global alignment offsets
   horizontalOffset: 0,
-  verticalOffset: 0,
+  verticalOffset: 200,
 
   // Refreshes parallax content on window load and resize
   responsive: true,
@@ -24,23 +28,26 @@ $.stellar({
   positionProperty: 'transform',
 
   // Enable or disable the two types of parallax
-  parallaxBackgrounds: true,
+  parallaxBackgrounds: false,
   parallaxElements: true,
 
   // Hide parallax elements that move outside the viewport
   hideDistantElements: false,
 
   // Customise how elements are shown and hidden
-  hideElement: function($elem) { $elem.hide(); },
-  showElement: function($elem) { $elem.show(); }
+  hideElement: function($elem) {
+      $elem.css('visibility', 'hidden');
+  },
+  showElement: function($elem) {
+      $elem.css('visibility', 'visible');
+  }
 });
 
-if(document.getElementById('container').classList.contains("page_home")) {
-  document.body.classList.toggle('home');
-}
-
+/*--------------------------------------------------------------
+# Add poster frame to video embed
+--------------------------------------------------------------*/
 // poster frame click event
-$(document).on('click','.js-videoPoster',function(ev) {
+$(document).on('click', '.js-videoPoster', function(ev) {
   ev.preventDefault();
   var $poster = $(this);
   var $wrapper = $poster.closest('.js-videoWrapper');
@@ -54,7 +61,7 @@ function videoPlay($wrapper) {
   // hide poster
   $wrapper.addClass('videoWrapperActive');
   // add iframe src in, starting the video
-  $iframe.attr('src',src);
+  $iframe.attr('src', src);
 }
 
 // stop the targeted/all videos (and re-instate the poster frames)
@@ -63,55 +70,56 @@ function videoStop($wrapper) {
   if (!$wrapper) {
     var $wrapper = $('.js-videoWrapper');
     var $iframe = $('.js-videoIframe');
-  // if we're stopping a particular video
+    // if we're stopping a particular video
   } else {
     var $iframe = $wrapper.find('.js-videoIframe');
   }
   // reveal poster
   $wrapper.removeClass('videoWrapperActive');
   // remove youtube link, stopping the video from playing in the background
-  $iframe.attr('src','');
+  $iframe.attr('src', '');
 }
 
-// Handles toggling the navigation menu for small screens
-( function() {
-	var container, button, menu, links, i, len;
+/*--------------------------------------------------------------
+# Handles toggling the navigation menu for small screens
+--------------------------------------------------------------*/
+(function() {
+  var container, button, menu, links, i, len;
 
-	container = document.getElementById( 'masthead' );
-	if ( ! container ) {
-		return;
-	}
+  container = document.getElementById('masthead');
+  if (!container) {
+    return;
+  }
 
-	button = container.getElementsByTagName( 'button' )[0];
-	if ( 'undefined' === typeof button ) {
-		return;
-	}
+  button = container.getElementsByTagName('button')[0];
+  if ('undefined' === typeof button) {
+    return;
+  }
 
-	menu = container.getElementsByTagName( 'ul' )[0];
+  menu = container.getElementsByTagName('ul')[0];
 
-	// Hide menu toggle button if menu is empty and return early.
-	if ( 'undefined' === typeof menu ) {
-		button.style.display = 'none';
-		return;
-	}
+  // Hide menu toggle button if menu is empty and return early.
+  if ('undefined' === typeof menu) {
+    button.style.display = 'none';
+    return;
+  }
 
-	menu.setAttribute( 'aria-expanded', 'false' );
-	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
-		menu.className += ' nav-menu';
-	}
+  menu.setAttribute('aria-expanded', 'false');
+  if (-1 === menu.className.indexOf('nav-menu')) {
+    menu.className += ' nav-menu';
+  }
 
-	button.onclick = function() {
-		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
-			container.className = container.className.replace( ' toggled', '' );
-			button.setAttribute( 'aria-expanded', 'false' );
-			menu.setAttribute( 'aria-expanded', 'false' );
-			button.classList.toggle("is-active");
-		} else {
-			container.className += ' toggled';
-			button.setAttribute( 'aria-expanded', 'true' );
-			menu.setAttribute( 'aria-expanded', 'true' );
-			button.classList.toggle("is-active");
-		}
-	};
-
-} )();
+  button.onclick = function() {
+    if (-1 !== container.className.indexOf('toggled')) {
+      container.className = container.className.replace(' toggled', '');
+      button.setAttribute('aria-expanded', 'false');
+      menu.setAttribute('aria-expanded', 'false');
+      button.classList.toggle("is-active");
+    } else {
+      container.className += ' toggled';
+      button.setAttribute('aria-expanded', 'true');
+      menu.setAttribute('aria-expanded', 'true');
+      button.classList.toggle("is-active");
+    }
+  };
+})();
